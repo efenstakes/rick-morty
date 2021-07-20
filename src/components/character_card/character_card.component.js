f730ab0cfb6485452a66ccd41314a3fd1850a6d3
Efen Stakes <efenstakes101@gmail.com>
2021-07-20 20:44:04 +0300

    add card styles and layout

--- src/components/character_card/character_card.component.js
+++ src/components/character_card/character_card.component.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import clsx from 'clsx'
 
 
 import './character_card.component.scss'
@@ -7,7 +8,31 @@ import './character_card.component.scss'
 const CharacterCardComponent = ({ character }) => {
     return (
         <div className="character_card">
-            { character.name }
+            <img 
+                src={character.image} 
+                alt={character.name}
+                className="character_card__image"
+            />
+
+            <div className="character_card__content">
+                <div className="character_card__content__name_wrap">
+                    <h3>
+                        {character.name}
+                    </h3>
+                    <div className={
+                        clsx([
+                            "character_card__content__name_wrap__status",
+                            {
+                                "character_card__content__name_wrap__status__dead": character.status != "Alive"
+                            }
+                        ])
+                    }
+                    />
+                </div>
+                <p>
+                    {character.species}
+                </p>
+            </div>
         </div>
     )
 }
