ef1d960d96d16d69a49dd7a6a7e2c1c189ccc456
Efen Stakes <efenstakes101@gmail.com>
2021-07-20 20:42:37 +0300

    add appbar styles

--- src/components/appbar/appbar.component.js
+++ src/components/appbar/appbar.component.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { IoPlayCircleSharp } from 'react-icons/io5'
 
 import './appbar.component.scss'
 
 const AppbarComponent = () => {
     return (
-        <div>
+        <div className="appbar">
+
+            <Link className="app_link" to="/">
+                <h3 className="appbar__logo">
+                    ROMYS
+                </h3>
+            </Link>
             
+            <div>
+                <IoPlayCircleSharp  className="appbar__icon" />
+            </div>
         </div>
     )
 }
