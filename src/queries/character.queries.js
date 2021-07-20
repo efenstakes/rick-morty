68e82ad86bb1d36d0c43a34ebac21df37b99d159
Efen Stakes <efenstakes101@gmail.com>
2021-07-20 18:03:45 +0300

    add app queries

---
+++ src/queries/character.queries.js
@@ -0,0 +1,28 @@
+import { gql } from '@apollo/client'
+
+const GET_CHARACTERS = gql`
+    query {
+        characters {
+        results {
+            id 
+            name
+            status
+            species
+            origin {
+            name
+            }
+            gender
+            origin {
+            name
+            }
+            image
+        }
+        }
+    }
+`;
+
+
+
+export default {
+    GET_CHARACTERS,
+}
