713d6d709b7cc85a08c4e91f539a6aafdc587640
Efen Stakes <efenstakes101@gmail.com>
2021-07-20 20:44:42 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 import HomePage from "./pages/home/home.page";
 
 function App() {
+
   return (
-    <HomePage />
-  );
+    <Router>
+      <Switch>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
