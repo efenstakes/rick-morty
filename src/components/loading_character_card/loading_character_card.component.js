e02c6bf9b335d91ddde7e096c8beb69ed1e2845d
Efen Stakes <efenstakes101@gmail.com>
2021-07-21 12:55:29 +0300

    add character loading card

---
+++ src/components/loading_character_card/loading_character_card.component.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+import './loading_character_card.component.scss'
+
+const LoadingCharacterCardComponent = () => {
+    return (
+        <div className="loading_character_card" />
+    )
+}
+
+
+export default LoadingCharacterCardComponent
